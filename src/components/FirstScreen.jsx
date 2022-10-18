87cbce3f6716afa44d8758ea4ed6a2f570bb2df4
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-10-18 11:27:24 +0100

    Initial set up of the portfolio project

---
+++ src/components/FirstScreen.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import displayPicture from '../assets/displayPicture@1x.png'
+
+const Wrapper = styled.div`
+  background-color: #27323E;
+  height: 1027px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  color: #FFFFFF;
+  margin: 0px;
+  text-align: center;
+`;
+
+const SubTitle = styled.h2`
+  font-size: 2rem;
+  color: #FFFFFF;
+  text-align: center;
+  padding-top: 64px;
+`;
+
+const DisplayPicture = styled.img`
+  width: 100%;
+  @media (min-width: 436px) {
+    width: 322px;
+  }
+`;
+
+export default function firstScreen() {
+  return (
+    <Wrapper>
+      <DisplayPicture src={displayPicture} />
+      <Title>
+        HARRIET BLUNDELL
+      </Title>
+      <SubTitle>
+        SOFTWARE ENGINEER
+      </SubTitle>
+    </Wrapper>
+  )
+}
