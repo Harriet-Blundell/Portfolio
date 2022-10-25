a55e968d10d28d35bc5df1624d1960de1961fbb0
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-10-25 08:55:00 +0100

    Update ThirdScreen.jsx
    WIP - Add alt tag in the array object

--- src/components/ThirdScreen.jsx
+++ src/components/ThirdScreen.jsx
@@ -26,17 +26,50 @@ const Title = styled.h1`
 
 export default function ThirdScreen() {
   const skillsImages = [
-    javaScriptLogo,
-    nextJsLogo,
-    nodeLogo,
-    html5Logo,
-    cssLogo,
-    sassLogo,
-    expressLogo,
-    contentfulLogo,
-    reactLogo,
-    handlebarsLogo,
-    gatsbyLogo,
+    {
+      imageSrc: javaScriptLogo,
+      imageAlt: 'JavaScript Logo'
+    },
+    {
+      imageSrc: nextJsLogo,
+      imageAlt: 'NextJS Logo'
+    },
+    {
+      imageSrc: nodeLogo,
+      imageAlt: 'Node Logo'
+    },
+    {
+      imageSrc: html5Logo,
+      imageAlt: 'HTML 5 Logo'
+    },
+    {
+      imageSrc: cssLogo,
+      imageAlt: 'CSS Logo'
+    },
+    {
+      imageSrc: sassLogo,
+      imageAlt: 'SASS Logo'
+    },
+    {
+      imageSrc: expressLogo,
+      imageAlt: 'Express Logo'
+    },
+    {
+      imageSrc: contentfulLogo,
+      imageAlt: 'Contentful Logo'
+    },
+    {
+      imageSrc: reactLogo,
+      imageAlt: 'React Logo'
+    },
+    {
+      imageSrc: handlebarsLogo,
+      imageAlt: 'Handlebars Logo'
+    },
+    {
+      imageSrc: gatsbyLogo,
+      imageAlt: 'Gatsby Logo'
+    }
   ];
 
   return (
@@ -45,13 +78,12 @@ export default function ThirdScreen() {
       <Swiper
         spaceBetween={5}
         slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        pagination={{ clickable: true, dynamicBullets: true }}
       >
-        {skillsImages.map((img, i) => {
+        {skillsImages.map((item, i) => {
           return (
             <SwiperSlide>
-              <img src={img} alt={img}></img>
+              <img src={item.imageSrc} alt={item.imageAlt}></img>
             </SwiperSlide>
           );
         })}
