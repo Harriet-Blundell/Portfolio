e349a3ad0d9625192bfcd2a9e41da1e2beaaab44
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-11-19 21:04:42 +0000

    Refactor code to get the swiper to function; comment out unnecessary styling

--- src/components/ProjectsScreen.jsx
+++ src/components/ProjectsScreen.jsx
@@ -1,63 +1,45 @@
 import React from "react";
-import projectImg from "../assets/learn-coding-online-for-free 1.png";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Wrapper, Title } from "../styles/SharedStyles.styled.js";
 import {
-  Container,
   Box,
-  InnerBox,
   Content,
   ProjectTitle,
   BottomText,
   ProjectImage,
 } from "../styles/ProjectsScreen.styled.js";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { projectsData } from "../utils";
 import "../index.css";
 import "swiper/css";
 
 export default function ProjectsScreen() {
   return (
-    <Wrapper
-      justifyContent="space-evenly"
-      alignItem="center"
-      flexDirection="column"
-    >
-      <Title>PROJECTS</Title>
+    <Wrapper alignItem="flex-start" justifyContent="center" flexWrap="wrap">
+      <Title paddingTop="100px">PROJECTS</Title>
       <Swiper
-        
         grabCursor={true}
-        slidesPerView={2}
+        breakpoints={{
+          436: {
+            slidesPerView: 1,
+          },
+          1125: {
+            slidesPerView: 2,
+          },
+        }}
       >
-        <Container>
-          <SwiperSlide className="swiper-slide-container">
-            <Box>
-              <ProjectImage src={projectImg} />
-              <InnerBox>
-                <ProjectTitle>Box 1</ProjectTitle>
-                <Content>
-                  Lorem Ipsum has been the industry's standard dummy text ever
-                  since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book.
-                </Content>
-                <BottomText>Read more</BottomText>
-              </InnerBox>
-            </Box>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide-container">
-            <Box>
-              <ProjectImage src={projectImg} />
-              <ProjectTitle>Box 2</ProjectTitle>
-              <Content>
-                Lorem Ipsum has been the industry's standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book.
-              </Content>
-              <BottomText>Read more</BottomText>
-            </Box>
-          </SwiperSlide>
-        </Container>
+        {projectsData.map((project) => {
+          return (
+            <SwiperSlide className="swiper-slide-container">
+              <Box>
+                <ProjectImage src={project.imageSrc} />
+                <ProjectTitle>{project.title}</ProjectTitle>
+                <Content>{project.info}</Content>
+                <BottomText>{project.moreInfo}</BottomText>
+              </Box>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </Wrapper>
   );
 }
-
-// Separate out box component to make the code cleaner?
--- src/styles/ProjectsScreen.styled.js
+++ src/styles/ProjectsScreen.styled.js
@@ -1,33 +1,35 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  width: 100vw;
-  font-family: "Montserrat", sans-serif;
-  @media (min-width: 1154px) {
-    flex-direction: row;
-  }
-`;
+// export const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+//   font-family: "Montserrat", sans-serif;
+//   @media (min-width: 1154px) {
+//     flex-direction: row;
+//   }
+// `;
 
 export const Box = styled.div`
   display: flex;
   justify-content: flex-end;
   flex-direction: column;
-  width: 519px;
   background-color: #ffffff;
   border-radius: 10px;
   padding: 15px;
+  width: 100%;
+  @media (min-width: 768px) {
+    width: 500px;
+  }
 `;
 
-export const InnerBox = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  flex-direction: column;
-  width: 519px;
-  background-color: #ffffff;
-`;
+// export const InnerBox = styled.div`
+//   display: flex;
+//   justify-content: flex-end;
+//   flex-direction: column;
+//   width: 519px;
+//   background-color: #ffffff;
+// `;
 
 export const ProjectTitle = styled.h1`
   font-size: 1.5rem;
