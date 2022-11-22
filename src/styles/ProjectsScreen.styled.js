3a98aa824ab30ccc281318458543b68d182109b5
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-11-22 18:51:35 +0000

    Update ProjectsScreen.styled.js

--- src/styles/ProjectsScreen.styled.js
+++ src/styles/ProjectsScreen.styled.js
@@ -1,15 +1,5 @@
 import styled from "styled-components";
 
-// export const Container = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: space-around;
-//   font-family: "Montserrat", sans-serif;
-//   @media (min-width: 1154px) {
-//     flex-direction: row;
-//   }
-// `;
-
 export const Box = styled.div`
   display: flex;
   justify-content: flex-end;
@@ -22,14 +12,6 @@ export const Box = styled.div`
     width: 500px;
   }
 `;
-
-// export const InnerBox = styled.div`
-//   display: flex;
-//   justify-content: flex-end;
-//   flex-direction: column;
-//   width: 519px;
-//   background-color: #ffffff;
-// `;
 
 export const ProjectTitle = styled.h1`
   font-size: 1.5rem;
