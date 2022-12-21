d6a429632930d5327df1c913fdb1f50bfb896e37
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-12-21 21:46:45 +0000

    change breakpoint number

--- src/components/ProjectsScreen.jsx
+++ src/components/ProjectsScreen.jsx
@@ -22,7 +22,7 @@ export default function ProjectsScreen() {
           436: {
             slidesPerView: 1,
           },
-          1125: {
+          1025: {
             slidesPerView: 2,
           },
         }}
